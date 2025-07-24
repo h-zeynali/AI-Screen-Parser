646ee52a8b5bcf929667232531fc7115f64f9d1c
Hossein Zeynali <zeynali@gmail.com>
2025-07-24 01:59:12 -0700

    add 30 sport shoes for better testing and demo

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,20 +4,7 @@ import products from "./data/products";
 import ProductGrid from "./components/ProductGrid";
 import useVisibleProducts from "./hooks/useVisibleProducts";
 
-function getPositionLabel(rect, winWidth, winHeight) {
-  const centerX = rect.left + rect.width / 2;
-  const centerY = rect.top + rect.height / 2;
 
-  let vertical = centerY < winHeight / 3 ? "top"
-               : centerY < (2 * winHeight) / 3 ? "middle"
-               : "bottom";
-
-  let horizontal = centerX < winWidth / 3 ? "left"
-                 : centerX < (2 * winWidth) / 3 ? "center"
-                 : "right";
-
-  return `${vertical} ${horizontal}`;
-}
 
 
 
